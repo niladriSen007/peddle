ec4cb4816181e709901afd68545a180983623ada
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2022-11-23 18:53:45 +0530

    Add files via upload

---
+++ peddle/index.js
@@ -0,0 +1,6 @@
+
+// const key=document.querySelector('.navbar-toggler-icon');
+// key.addEventListener("click",function(){
+//     console.log("In key");
+// document.querySelector('.navbar').classList.toggle('navbar-new');
+// });
